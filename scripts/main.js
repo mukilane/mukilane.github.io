04cb490bb4fe77e0fba46a857c62b2a7f20e4ec6
mukilane <mukilane@gmail.com>
2016-11-28 12:27:03 +0530

    Build 28N (Routing)

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 var app = angular.module('port', ['ngMaterial', 'ngRoute']);
 
 app.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
+  	$mdThemingProvider.theme('default')
     .accentPalette('blue');
 });
+app.config(function($locationProvider) {
+	$locationProvider.html5Mode(true);
+});
 
-app.controller('main', function ($scope, $interval) {
+app.controller('main', function ($scope, $interval, $http, $compile, $location, $window) {
 	$scope.counter = 0;
 	$scope.adjective = ["A Web Developer", "A Programmer", "Google Play Rising Star", "Loves OpenSource", "A Linux Admin", "Elon Musk Fan", "Philomath", "Tech Enthusiast"];
 	$interval(function() { 
@@ -15,12 +18,26 @@ app.controller('main', function ($scope, $interval) {
 			$scope.counter = 0;
 		}
 	}, 5000);
+	$scope.goto = function(dest) {
+		$.get(dest , function( response ) {
+		 	var elem  = $(response).filter('#main');
+			$scope.target = angular.element(document).find('md-content');
+			$scope.target.html(elem);
+			$location.path(dest);
+			$compile($scope.target.contents())($scope);
+		});	
+	};
+
+	$scope.go = function (dest) {
+		$window.location.href = dest;
+	}
 });
+
 
 app.directive('tileImage', function () {
     return {
       restrict: 'E',
-      template: "<img src='{{source}}' style='opacity:{{opacity}}; width: {{width}};'/>",
+      template: "<img ng-src='{{source}}' style='opacity:{{opacity}}; width: {{width}};'/>",
       scope: {
         source: '@',
         opacity: '@',
