7ee7fd3d565e8392b07333654437a1bb45d5016a
mukilane <mukilane@gmail.com>
2016-12-06 20:54:02 +0530

    Build 6D.10 (NPM & Gulp init)

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+
+const child = require('child_process');
+const gutil = require('gulp-util');
+
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['serve']);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('default', ['jekyll']);
