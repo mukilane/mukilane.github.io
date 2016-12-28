3d29438c967c200307904f9f7a535af44ecfdc00
mukilane <mukilane@gmail.com>
2016-12-28 16:56:54 +0530

    Build 28D.1.1

--- scripts/main.js
+++ scripts/main.js
@@ -21,13 +21,13 @@ if ('serviceWorker' in navigator) { // If Service worker feature is available in
 						if (navigator.serviceWorker.controller) {
 							console.log('[SW] Registration successful');
 							// Any old content will be purged and new content will be added to the cache
-							angular.element(document.getElementById('ctrl')).scope().swToast('New content available. Refresh to see', true);
+							angular.element(document.getElementById('ctrl')).scope().swToast('New content available. Refresh to see', 'refresh');
 							// Refresh is done to load overcome the network first strategy.
 							// Content will be loaded from the cache only the second time
 							// The content won't be offline ready at the first visit.
 						} else {
 							// If controller is null, then caching is complete
-							angular.element(document.getElementById('ctrl')).scope().swToast('Content cached for offline use', false);
+							angular.element(document.getElementById('ctrl')).scope().swToast('Content cached for offline use', 'ok');
 						}
 						break;
 					case 'redundant':
