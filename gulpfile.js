2dbcb2561f19d3c6e51716629645283786a4ab21
mukilane <mukilane@gmail.com>
2017-01-15 18:43:22 +0530

    Build 15J.2 (Amp build process using gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-/* Gulp Build */
+/* Build Process */
 
 const gulp = require('gulp');
 
 const child = require('child_process');
 const gutil = require('gulp-util');
 
+// Jekyll Build
 gulp.task('jekyll', () => {
-  const jekyll = child.spawn('jekyll', ['serve']);
+  const jekyll = child.spawn('jekyll', ['build']);
 
   const jekyllLogger = (buffer) => {
     buffer.toString()
@@ -15,7 +16,13 @@ gulp.task('jekyll', () => {
   };
 
   jekyll.stdout.on('data', jekyllLogger);
-  jekyll.stderr.on('data', jekyllLogger);
+  // jekyll.stderr.on('data', jekyllLogger);
 });
 
-gulp.task('default', ['jekyll']);
+// Copy generated amp files to the source
+// Done because of Github's restriction on custom plugins
+gulp.task('amp', () => {
+	gulp.src('_site/amp/blog/**/*.html').pipe(gulp.dest('amp'));
+});
+
+gulp.task('default', ['jekyll','amp']);
