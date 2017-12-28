15b4eefd3eb08cba72a80d0ea137512dbae9ce61
mukilane <mukilane@gmail.com>
2017-12-28 16:19:12 +0530

    Build 28D.1 (Push notifications setup)

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,49 @@
 var functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send("Hello from Firebase!");
+	let token;
+	admin.firestore().collection("users").doc("data").get()
+		.then( data => {
+			token = data.data()['notificationTokens'];
+			response.send(token);
+		})
+	
 });
 
 exports.sendNotification = functions.database.ref('/data/posts').onWrite( event => {
-	let tokens = "cTaUcjx9H80:APA91bEBJsjhti2Wwnsc40B8Fe7gC08yjAAYeD…xpabLnSP2RfcoAQNmS6gt7lA7uCdxpYRUaPK728YfF5gGQbe9";
-	// const token = admin.database().ref('/data/users/notificationToken/').once('value', (data) => tokens = data);
-	
-	const payload = {
-		notification: {
-			title: "Test",
-			body: "New article available"
-		}
-	};
-	admin.messaging().sendToDevice(tokens, payload).then(response => {
-		console.log('Notification sent:', response);
-	}).catch(error => {
-		console.log(error);
-	})
+	// let tokens = "cTaUcjx9H80:APA91bEBJsjhti2Wwnsc40B8Fe7gC08yjAAYeD…xpabLnSP2RfcoAQNmS6gt7lA7uCdxpYRUaPK728YfF5gGQbe9";
+	// admin.firestore().collection("users").doc("data").get()
+	// 	.then(data => {
+	// 		console.log(data.data());
+	// 	});
+	// const payload = {
+	// 	notification: {
+	// 		title: "Test",
+	// 		body: "New article available"
+	// 	}
+	// };
+	// admin.messaging().sendToDevice(this.tokens, payload).then(response => {
+	// 	console.log('Notification sent:', response);
+	// }).catch(error => {
+	// 	console.log(error);
+	// });
+	admin.firestore().collection("users").doc("data").get()
+		.then(data => {
+			const tokens = data.data()['notificationTokens'];
+			const payload = {
+				notification: {
+					title: "Test",
+					body: "Notification works"
+				}
+			};
+			admin.messaging().sendToDevice(tokens, payload)
+				.then(response => {
+					console.log("Notification sent:", response);
+				}).catch(error => {
+					console.log(error);
+				})
 
+		});
 });
