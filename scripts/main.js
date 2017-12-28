145531c17bf0f0b44f20115c5bce85b296ed170b
mukilane <mukilane@gmail.com>
2017-12-28 16:19:12 +0530

    Build 28D.1 (Push notifications setup)

--- scripts/main.js
+++ scripts/main.js
@@ -162,28 +162,54 @@ app.controller('feedback', function ($scope, $firebaseObject, $firebaseAuth, Dia
 });
 
 // Controller for Notifications
-app.controller('notifications', function($scope, $firebaseObject) {
+app.controller('notifications', function($scope, $firebaseObject, Toast) {
 	"ngInject";
+	$scope.isNotificationEnabled = false;
+
+	// Store the state of notification permission in LocalStorage
+	if(localStorage.getItem('isNotificationEnabled')) { 
+		$scope.isNotificationEnabled = true;
+	}
+
+	$scope.setEnabled = () => {
+		localStorage.setItem('isNotificationEnabled', 'true');
+		$scope.isNotificationEnabled = true;
+	};
+	
 	const messaging = firebase.messaging();
+
 	$scope.enableNotifications = () => {
 		messaging.requestPermission()
 			.then(() => {
-				console.log('Granted');
+				Toast('Notifications are enabled');
+				$scope.setEnabled();
 				$scope.getToken();
 			})
-			.catch((err) => console.log(err));
-	}
+			.catch((err) => {
+				console.log(err);
+				Toast('Notifications blocked!. To enable, go to site settings.');
+			});
+	};
+	
 	$scope.getToken = () => {
 		messaging.getToken()
 			.then((currentToken) => {
 				if(currentToken) {
-					firebase.database().ref('data/users/notificationToken/').push(currentToken);
-					firebase.database().ref('/data/users/notificationToken').once('value')
-						.then((data) => console.log(data.val()));
+					let tokens = [];
+					// Add Tokens to Firestore
+					firebase.firestore().collection("users").doc("data").get()
+					.then(data => {
+							tokens = data.data()['notificationTokens'];
+							tokens.push(currentToken);
+							firebase.firestore().collection("users").doc("data").update({
+								'notificationTokens': tokens
+							});
+					});
 				}
 			})
 	}
-	messaging.onMessage((payload) => console.log(payload));
+	// If a Push Notification is received when the app is active, display a Toast
+	messaging.onMessage((payload) => Toast(payload));
 });
 
 // Controller for search
