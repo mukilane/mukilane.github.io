992608917ba6829ba45b33f9f05eb0f2cee0344e
mukilane <mukilane@gmail.com>
2018-05-17 23:47:16 +0530

    contact logic completed

--- scripts/main.js
+++ scripts/main.js
@@ -116,6 +116,7 @@ app.controller("ContactCtrl", function($scope, Panel, ContactService) {
 		email: "",
 		password: ""
 	};
+	$scope.user = {};
 	$scope.createUser = () => {
 		firebase.auth().createUserWithEmailAndPassword($scope.data.email, $scope.data.password)
 			.catch(function(error) {
@@ -127,14 +128,14 @@ app.controller("ContactCtrl", function($scope, Panel, ContactService) {
 		firebase.auth().signInWithEmailAndPassword($scope.data.email, $scope.data.password)
 			.catch(function(error) {
 			  	var errorCode = error.code;
-		  		var errorMessage = error.message;	  
+		  		var errorMessage = error.message;
 			});
 	}
 	$scope.signout = () => {
 		firebase.auth().signOut().then(function() {
 		  	$scope.showForm = false;
 		}).catch(function(error) {
-		  // An error happened.	
+		  // An error happened.	S
 		});
 	}
 	$scope.newForm = () => {
@@ -147,54 +148,33 @@ app.controller("ContactCtrl", function($scope, Panel, ContactService) {
 	}
 	firebase.auth().onAuthStateChanged(function(user) {
 	  if (user) {
-	    // User is signed in.
-	    
 	    $scope.showForm = true;
-	    var displayName = user.displayName;
-	    var email = user.email;
-	    var emailVerified = user.emailVerified;
-	    var photoURL = user.photoURL;
-	    var isAnonymous = user.isAnonymous;
-	    var uid = user.uid;
-	    var providerData = user.providerData;
+	    $scope.user = user;
 	    ContactService.isNewForm().then(data => $scope.isNewForm = data);
 	  } else {
 	    // User is signed out.
-	    // ...
 	  }
 	});
 });
 
-app.controller('ContactFormCtrl', function($scope, ContactService) {
+app.controller('ContactFormCtrl', function($scope, ContactService, $timeout, Toast) {
 	$scope.existingForm = ContactService.useExistingForm();
-	$scope.contact = ContactService.data;
-
-	// var user = firebase.auth().currentUser;
-	// var ref = firestore.collection('contacts');
-	// $scope.merge = false; // New Doc
-	// if(user !== null) {
-	// 	ref.doc(user.uid).get()
-	// 		.then(doc => {
-	// 			if(doc.exists) {
-	// 				$scope.contact = doc.data();
-	// 				$scope.merge = true; // Existing doc
-	// 			} else {
-	// 				console.log("No doc");
-	// 			}
-	// 		}).catch(error => {
-	// 			console.log(error);
-	// 		})
-	// } else {
-	// 	console.log("Not signed in");
-	// }
-	// $scope.contact = {};
-	$scope.submit = () => {
-		ContactService.update($scope.contact);
-		// ref.doc(user.uid).collection('form').doc().set($scope.contact, { merge: $scope.merge })
-		// 	.then(() => console.log("Done"))
-		// 	.catch(err => console.log(err));
+	$scope.contact = {};
+	$scope.forms = [];
+	$scope.selectedForm = {}; // Todo
+	ContactService.getForms().then((data) => $scope.forms = data );
+	$scope.selectForm = () => {
+		console.log($scope.contact.id);
+		ContactService.selectForm($scope.contact.id);
 	}
-
+	$scope.submit = (close) => {
+		ContactService.update($scope.contact)
+			.then(function() {
+				console.log("set out");
+				Toast("Done. Reply soon!")
+				close();
+			});
+	}
 });
 
 app.service('ContactService', function($timeout) {
@@ -204,9 +184,20 @@ app.service('ContactService', function($timeout) {
 	var user = null;
 	var ref = null;
 	var existingForm = false;
+	var forms = [];
+	var _isNewForm = true;
 	var useExistingForm = function() {
 		return existingForm;
 	};
+	var getForms = function() {
+		forms = [];
+		return ref.collection('forms').get().then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				forms.push(doc.data());
+			});
+			return forms;
+		});
+	}
 	var isNewForm = function() {
 		user = firebase.auth().currentUser;
 		ref = firestore.collection('contacts').doc(user.uid);
@@ -214,31 +205,35 @@ app.service('ContactService', function($timeout) {
 			.then((_doc) => {
 				doc = _doc.data();
 				if(doc.formCount > 0) {
+					_isNewForm = false;
 					return false;
 				} else {
+					_isNewForm = true;
 					return true;
 				}
 			});
 	}
 
-	var selectForm = function(formOrdinal) {
-		selectedForm = doc.formList[formOrdinal];
+	var selectForm = function(id) {
+		selectedForm = id;
+		console.log(selectedForm);
 	}
 
 	var setForm = function(e) {
-		existingForm = e;
+		_isNewForm = !e;
+		existingForm = e; // Whether existing form is to be used : Bool
 	}
 
 	var update = function(data) {
 		var docRef = null;
-		
-		if (isNewForm) {
+		var timestamp = new Date();
+		if (_isNewForm) {
 			docRef = ref.collection('forms').doc();
 			doc.formCount = doc.formCount + 1;
 			doc.formList.push({
 				id: docRef.id,
 				title: data.title,
-				timestamp: new Date()
+				timestamp: timestamp
 			});
 		} else {
 			docRef = ref.collection('forms').doc(selectedForm);
@@ -246,21 +241,23 @@ app.service('ContactService', function($timeout) {
 		ref.set({
 			formCount : doc.formCount,
 			formList: doc.formList
-		}, {}).then(function() { console.log("set"); });
-		return docRef.set(data, {merge: true})
-			.then(function() {
-				console.log("set out");
-			});
-	
+		}, { merge: true }).then(function() { console.log("set"); });
+
+		data.timestamp = timestamp;
+		data.id = docRef.id;
+		delete data.$$hashKey;
+		delete data.$$mdSelectId;
+		
+		return docRef.set(data, {merge: true});
 	} 
 	return {
 		isNewForm: isNewForm,
 		selectedForm: selectedForm,
 		selectForm: selectForm,
 		update: update,
-		data: doc,
 		setForm: setForm,
-		useExistingForm: useExistingForm
+		useExistingForm: useExistingForm,
+		getForms: getForms
 	}
 });
 // Controller for feeback form
